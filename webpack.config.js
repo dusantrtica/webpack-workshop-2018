0c3d6fefed54fc2b901a73698aae108e321d5bdd
Dusan Trtica <dtrtica@gogoair.com>
2018-07-22 23:37:35 +0200

    Added plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  mode: "none"
-};
+// razni utils pluginovi koji dolaze za webpackom,
+// npr. za progress
+const webpack = require('webpack');
+
+// Ovaj plugin nam kreira index.html i injectuje u njega
+// sve sto je output-ovano iz webpacka (bundle.js, css...)
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = ({mode}) => {
+	console.log(mode);
+	return {
+		mode,
+		output: {
+			filename: 'bundle.js'
+		},
+		plugins: [
+			new HtmlWebpackPlugin(),
+			new webpack.ProgressPlugin()
+		]
+	}
+}
